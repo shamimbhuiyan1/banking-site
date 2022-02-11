66e2f82117c37cd7ab3cb61eca889d755c73619e
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-02-11 19:20:16 +0600

    deposit input

---
+++ js/banking.js
@@ -0,0 +1,5 @@
+document.getElementById('deposit-button').addEventListener('click',function(){
+    const depositInput = document.getElementById('deposit-input');
+    depositAmount = depositInput.value;
+    console.log(depositAmount);
+})
