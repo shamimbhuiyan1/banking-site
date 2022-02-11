3934efc5948233006d5e1b4304862e59b08ab6bf
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-02-11 20:23:35 +0600

    updated balance part

--- js/banking.js
+++ js/banking.js
@@ -1,5 +1,25 @@
 document.getElementById('deposit-button').addEventListener('click',function(){
     const depositInput = document.getElementById('deposit-input');
-    depositAmount = depositInput.value;
-    console.log(depositAmount);
+    const depositAmountText = depositInput.value;
+    const depositAmount =parseFloat(depositAmountText);
+    
+
+    // get current deposit 
+
+    const depositTotal = document.getElementById('deposit-total');
+    const depositTotalText = depositTotal.innerText;
+    const previousDepositTotal = parseFloat(depositTotalText);
+    
+
+     depositTotal.innerText=previousDepositTotal+depositAmount ;
+    
+    // update balance 
+    const balanceTotal=document.getElementById('balance-total');
+    const balanceTotalText=balanceTotal.innerText;
+    const  previousBalanceTotal=parseFloat(balanceTotalText);
+    balanceTotal.innerText= previousBalanceTotal+depositAmount;
+    // clear input value
+    depositInput.value ='';
+
+
 })
